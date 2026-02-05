abb89b7864f13674b317755705e72ec28735669b
william.baker@omsmeasure.com <william.baker@omsmeasure.com>
2026-02-05 11:15:21 +0000

    Update for feature 2

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <string>
 
 #include "feat1/Submodule_POC/feature1-include.hpp"
+#include "feat2/Submodule_POC/feature2-include.hpp"
 
 using namespace std;
 
 int main()
 {
-    vector<string> msg {FEATURE_1};
+    vector<string> msg {FEATURE_1, FEATURE_2};
 
     for (const string& word : msg)
     {
